bc6752199598fa814245563c3fedd9265f4581f2
SoominYim <soo15soo@kakao.com>
2024-10-21 17:15:14 +0900

    feat : index.js update 정리

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // chapter_03
-// import Library from "./chapter_03/Library.jsx";
+/* import Library from "./chapter_03/Library.jsx";
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); */
 
 // chapter_04
-import Clock from "./chapter_04/Clock";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
+/*import Clock from "./chapter_04/Clock";
 setInterval(() => {
   root.render(
     <React.StrictMode>
@@ -19,6 +22,13 @@ setInterval(() => {
     </React.StrictMode>
   );
 }, 1000);
+*/
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
