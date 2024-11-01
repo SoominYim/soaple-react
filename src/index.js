b5ce081b9e89dc2068a57fc2da2c837d32077917
SoominYim <soo15soo@kakao.com>
2024-11-01 17:59:10 +0900

    feat : chapter_06 update State , Lifecicle 실습

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Welcome from "./chapter_05_01/Welcome";
+import NotificationList from "./chapter_06/NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -13,32 +13,35 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-); */
+    </React.StrictMode>
+    ); */
 
 // chapter_04
 
 /*import Clock from "./chapter_04/Clock";
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
-*/
+    setInterval(() => {
+      root.render(
+        <React.StrictMode>
+        <Clock />
+        </React.StrictMode>
+        );
+        }, 1000);
+        */
 
 // chapter_05
 
 /* import CommentList from "./chapter_05/CommentList";
+       
+       root.render( 
+        <React.StrictMode>
+        <CommentList></CommentList>
+        </React.StrictMode>
+        )
+        */
 
-root.render( 
-    <React.StrictMode>
-      <CommentList></CommentList>
-    </React.StrictMode>
-)
-*/
-
+// chapter_05_01
+/*
+// import Welcome from "./chapter_05_01/Welcome";
 const App = (props) => {
   return (
     <div>
@@ -48,10 +51,13 @@ const App = (props) => {
     </div>
   );
 };
+*/
+
+root.render(<React.StrictMode></React.StrictMode>);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <NotificationList />
   </React.StrictMode>
 );
 
