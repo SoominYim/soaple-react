804acc8fa95b90031c95c9f0ee1ae55d2c45bb31
SoominYim <soo15soo@kakao.com>
2024-11-04 14:21:16 +0900

    test : chapter_06 LifeCycle test code 작성

--- src/chapter_06/NotificationList.jsx
+++ src/chapter_06/NotificationList.jsx
@@ -3,12 +3,15 @@ import Notification from "./Notification";
 
 const reserveNotifications = [
   {
+    id: 1,
     message: "안녕하세요, 오늘 일정을 알려드립니다.",
   },
   {
+    id: 2,
     message: "점심식사 시간입니다.",
   },
   {
+    id: 3,
     message: "이제 곧 마감이 시작됩니다.",
   },
 ];
@@ -27,7 +30,6 @@ class NotificationList extends React.Component {
   componentDidMount() {
     const { notifications } = this.state;
     timer = setInterval(() => {
-      console.log(notifications.length);
       if (notifications.length < reserveNotifications.length) {
         const index = notifications.length;
         notifications.push(reserveNotifications[index]);
@@ -35,6 +37,9 @@ class NotificationList extends React.Component {
           notifications: notifications,
         });
       } else {
+        this.setState({
+          notifications: [],
+        });
         clearInterval(timer);
       }
     }, 1000);
@@ -44,7 +49,7 @@ class NotificationList extends React.Component {
     return (
       <div>
         {this.state.notifications.map((notification, i) => {
-          return <Notification key={i} message={notification.message} />;
+          return <Notification key={notification.id} id={notification.id} message={notification.message} />;
         })}
       </div>
     );
